bd103924bf6fe07367c6e6da3ba8b0700d65e921
your_github_username <youremail@domain.com>
2023-06-29 03:13:54 -0400

    okay

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,14 +1,14 @@
 from deep_translator import MyMemoryTranslator
 
 
-def englishtofrench(englishtext):
+def english_to_french(englishtext):
     '''This function translates the input to French'''
 
     frenchtext = MyMemoryTranslator(source = 'english', target = 'french').translate(englishtext)
     return frenchtext
 
 
-def frenchtoenglish(frenchtext):
+def french_to_english(frenchtext):
     '''This function translates the input to English'''
 
     englishtext = MyMemoryTranslator(source = 'french', target = 'english').translate(frenchtext)
