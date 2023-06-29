7509851cb50e445d736e4c2e03d96316b9886ea2
your_github_username <youremail@domain.com>
2023-06-29 03:06:03 -0400

    yes

--- final_project/server.py
+++ final_project/server.py
@@ -4,21 +4,25 @@ import json
 
 app = Flask("Web Translator")
 
+
 @app.route("/englishToFrench")
 def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')
+    engtext = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to French"
+    frentext = translator.english_to_french(engtext)
+    return frentext
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
-    textToTranslate = request.args.get('textToTranslate')
+    frentext = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to English"
+     engtext = translator.french_to_english(frentext)
+     return engtext
 
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
